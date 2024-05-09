af316a8b25ba16d977922bb68c37c3639374e8cf
1075611401 <1075611401@qq.com>
2024-05-09 17:42:34 +0800

    第二天

--- src/main/java/com/gelin/demo/DemoApplication.java
+++ src/main/java/com/gelin/demo/DemoApplication.java
@@ -6,7 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * 应用程序的主入口点。
+     *
+     * @param args 命令行参数，传递给SpringApplication的运行方法。
+     * @see SpringApplication#run(Class, String...)
+     */
     public static void main(String[] args) {
+        // 启动Spring应用
         SpringApplication.run(DemoApplication.class, args);
     }
 
