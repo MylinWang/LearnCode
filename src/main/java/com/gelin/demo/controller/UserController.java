fcb250a1a87f182cdf2d561ac886cb3fb20774ac
1075611401 <1075611401@qq.com>
2024-05-10 16:24:00 +0800

    第三天

---
+++ src/main/java/com/gelin/demo/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.gelin.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String getUser(){
+        return "user";
+    }
+
+
+}
