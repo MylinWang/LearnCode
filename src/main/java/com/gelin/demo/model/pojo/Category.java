70d77a24027fea1cbd3d6902b0f3d018e79e7bc1
1075611401 <1075611401@qq.com>
2024-05-19 21:18:02 +0800

    第十一天

---
+++ src/main/java/com/gelin/demo/model/pojo/Category.java
@@ -0,0 +1,55 @@
+package com.gelin.demo.model.pojo;
+
+import java.util.Date;
+
+public class Category {
+    private Integer id;
+
+    private String name;
+
+    private Integer parentId;
+
+    private Integer level;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
